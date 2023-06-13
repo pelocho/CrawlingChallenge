39f03e507bcc11b288b056b35abc23e67d20882d
Oscar Morente <oscarmorente98@gmail.com>
2023-06-13 15:32:09 +0200

    All fields extraction completed

--- CrawlingChallenge/CrawlingChallenge/spiders/adidas_spider.py
+++ CrawlingChallenge/CrawlingChallenge/spiders/adidas_spider.py
@@ -15,25 +15,32 @@ class AdidasSpider(CrawlSpider):
     rules = {
         Rule(LinkExtractor(allow = (), restrict_xpaths = ('//a/span[@data-auto-id="pagination-right-button"]'))),
         Rule(LinkExtractor(allow = (), restrict_xpaths = ('//a/div/p[@class="glass-product-card__title"]')),
-             callback = 'parse_item', follow = False)
+             callback = 'parse_item', follow = False),
+        Rule(LinkExtractor(allow = (), restrict_xpaths = ('//button[@data-auto-id="image-grid-expand-button"]'))),
     }
 
     def parse_item(self, response):
         item = ProductItem()
 
-        item['title'] = response.xpath('//div[@class="product-description___1TLpA"]/h1[@data-auto-id="product-title"]')
-        item['brand'] = response.xpath('//div[@data-auto-id="product-category"]')
-        
-        #TODO: Investigate this fields
-        #There's nothing like this field on the page of the product
-        #item['description'] = response.xpath('')
-        #item['id'] = response.xpath('')
-        #item['availability'] = response.xpath('')
+        # Example url -> https://www.adidas.es/zapatilla-gazelle/IG4986.html
+        # Getting id spliting the url until we get IG4986
+        item_url = response.request.url
+        item_id = item_url.split('/')[-1].split('.')[0]
+        original_price = response.xpath('//div[@class="product-price___2Mip5 gl-vspace"]/div[1]/div[1]/div[1]/div[1]').extract()
+        unique_color = response.xpath('//div[@data-auto-id="color-label"]').extract()
 
-        item['current_price'] = response.xpath('//div[@class="product-price___2Mip5 gl-vspace"]')
-        item['original_price'] = response.xpath()
-        item['images_urls'] = response.xpath('//picture/img/@src')
-        item['colors'] = response.xpath('//div[@class="color-chooser-grid___1ZBx_"]/a/img/@alt')
-        item['sizes'] = response.xpath('//div[@data-auto-id="size-selector"]/button/span/text()')
-        item['category_path'] = "".join(response.xpath('//div[1]/div[2]/ol/li[@typeof="ListItem"]'))
+        item['id'] = item_id
+        item['title'] = response.xpath('//div[@class="product-description___1TLpA"]/h1[@data-auto-id="product-title"]').extract()
+        item['brand'] = response.xpath('//div[@data-auto-id="product-category"]').extract()
+        item['description'] = response.xpath('//meta[@id="meta-og-description"]/@content').extract()
+        item['original_price'] = original_price
+        item['availability'] = response.xpath('//div[@data-auto-id="size-selector"]/button[@class="gl-label size___2lbev"]').extract()
+        item['images_urls'] = response.xpath('//picture/img/@src').extract()
+        item['sizes'] = response.xpath('//div[@data-auto-id="size-selector"]/button/span/text()').extract()
+        item['category_path'] = "".join(response.xpath('//div[1]/div[2]/ol/li[@typeof="ListItem"]')).extract()
+
+        # We need to check if there's a descount
+        item['current_price'] = response.xpath('//div[@class="product-price___2Mip5 gl-vspace"]/div[1]/div[1]/div[1]/div[2]').extract() or original_price
+        # We need to check if there's more colors
+        item['colors'] = response.xpath('//div[@class="color-chooser-grid___1ZBx_"]/a/img/@alt').extract() or unique_color
         
